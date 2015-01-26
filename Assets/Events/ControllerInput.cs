380df373439ce526db8b5761d9437441826e642a
brulo <bruce.lott@gmail.com>
2015-01-25 19:56:42 -0800

    Implement simple event demo

---
+++ Assets/Events/ControllerInput.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllerInput : MonoBehaviour {
+
+	public delegate void ButtonDown();
+	public static event ButtonDown OnJump;
+
+	void Update () {
+		if(Input.GetButtonDown("Jump")) {
+			// Don't send message if noone is listening.
+			if(OnJump != null) {
+				OnJump();
+			}
+		}
+	}
+
+}
---
+++ Assets/Events/TurnRed.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorScript : MonoBehaviour {
+
+	SpriteRenderer renderer;
+
+	// Overload these so this script stops listening when disabled.
+	void OnEnable() {
+		// Call FlipColor() when OnJump event is received.
+		ControllerInput.OnJump += FlipColor;
+	}
+
+	void OnDisable() {
+		ControllerInput.OnJump -= FlipColor;
+	}
+
+	// Grap the sprite renderer.
+	void Start () {
+		renderer = GetComponent<SpriteRenderer>();
+	}
+
+	// Flip sprite renderer between red and black.
+	void FlipColor() {
+		if(renderer.color == Color.red)
+			renderer.color = Color.black;
+		else 
+			renderer.color = Color.red;
+	}
+	
+}
