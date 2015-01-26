c03f2549c6e642d9befa0391f9b5a121f898475d
brulo <bruce.lott@gmail.com>
2015-01-25 20:30:00 -0800

    Implement event with float argument

--- Assets/Events/ControllerInput.cs
+++ Assets/Events/ControllerInput.cs
@@ -6,13 +6,24 @@ public class ControllerInput : MonoBehaviour {
 	public delegate void ButtonDown();
 	public static event ButtonDown OnJump;
 
+	public delegate void JoyAxis(float val);
+	public static event JoyAxis HorizontalJoyAxis;
+
 	void Update () {
+		// Jump 
 		if(Input.GetButtonDown("Jump")) {
 			// Don't send message if noone is listening.
 			if(OnJump != null) {
 				OnJump();
 			}
 		}
+
+		// Horizontal joystick axis
+		if(HorizontalJoyAxis != null) {
+			float horizontalMove = Input.GetAxis("Horizontal");
+			HorizontalJoyAxis(horizontalMove);	
+			Debug.Log(horizontalMove);
+		}	
 	}
 
 }
